d915d4f7fc6df39719a91b51e7a365afbff7f8b9
IrynaKniazieva <gleb0812@gmail.com>
2022-08-31 01:01:06 +0300

    Task - 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,42 @@
+import SimpleLightbox from "simplelightbox/dist/simple-lightbox.esm"
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+// 2.1. находим куда вставить разметку
+const listGallery = document.querySelector('.gallery');
+// 2.2. ~ создаем карточки
+const cardsGallery = createGalleryCard (galleryItems);
+// 2.3. добавляем их в ul (gallery)
+listGallery.insertAdjacentHTML('beforeend', cardsGallery); 
+
+
+// 1. создаем разметку
+function createGalleryCard (galleryItems) {
+    return galleryItems.map(( { preview, original, description } ) => {
+return `
+<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+</li>
+`
+    }).join('');
+}
+
+let gallery = new SimpleLightbox('.gallery a', {captionsData: "alt", captionDelay: 250,});
+gallery.on('show.simplelightbox', function () {
+	// do something…
+});
+
