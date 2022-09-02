ab76abcd03d27b4264fa06bd1dd99e9b8a52252d
IrynaKniazieva <gleb0812@gmail.com>
2022-09-03 00:43:51 +0300

    Task - 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,6 +1,42 @@
-import Player from @vimeo/player;
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
-const iframe = document.querySelector('#@vimeo/player');
-const player = new Vimeo.Player(iframe);
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
 
-player.on ('',);
+player.on('timeupdate', throttle(videoStopTime, 1000));
+
+function videoStopTime(timeupdate) {
+  let pause = timeupdate.seconds;
+  console.log(pause);
+  localStorage.setItem('videoplayer-current-time', pause);
+}
+
+// const currentTime = localStorage.getItem('videoplayer-current-time');
+// player
+//   .setCurrentTime(currentTime)
+//   .then(function (pause) {
+//     // seconds = the actual time that the player seeked to
+//   })
+//   .catch(function (error) {
+//     switch (error.name) {
+//       case 'RangeError':
+//         break;
+//       default:
+//         break;
+//     }
+//   });
+
+  player.setCurrentTime(30.456).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
